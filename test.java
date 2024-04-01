a8390841ed6c869734c3c1e15a06b05023951453
jieksz <72543442+jieksz@users.noreply.github.com>
2024-04-01 19:36:36 +0800

    test

---
+++ test.java
@@ -0,0 +1,30 @@
+public class test {
+    public static void main(String[] args) {
+        byte num1=127;
+        System.out.println(num1);
+
+        short num2=66;
+        System.out.println(num2);
+
+        int num3=88;
+        System.out.println(num3);
+
+        long num4=10000000;
+        System.out.println(num4);
+
+        float num5=5.5F;
+        System.out.println(num5);
+
+        double num6=8.8;
+        System.out.println(num6);
+
+        System.out.println("================");
+
+        char a='c';
+        System.out.println(a);
+
+        char a2=97;
+        System.out.println(a2);
+        System.out.println("================");
+    }
+}
