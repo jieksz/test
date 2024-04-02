1879abcc2eb693b835477996fbee6c14de38e7f2
jieksz <jieksz@163.com>
2024-04-02 14:36:59 +0800

    一次上课记录

---
+++ Demo07.java
@@ -0,0 +1,15 @@
+public class Demo07 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println(num);
+        {
+            System.out.println(num);
+        }
+
+        int i;
+        i = 5;
+        System.out.println(i);
+
+    }
+}
