dbd42eae392e31bb9c82a5f6ebee271f00302b07
jieksz <jieksz@163.com>
2024-04-02 17:13:04 +0800

    比较

---
+++ Demo14.java
@@ -0,0 +1,11 @@
+public class Demo14 {
+    public static void main(String[] args) {
+        boolean b = 5 > 3;
+        System.out.println(b);
+        System.out.println(5 < 3);
+        System.out.println(5 >= 5);
+        System.out.println(3 <= 4);
+        System.out.println(10 == 11);
+        System.out.println(11 != 11);
+    }
+}
