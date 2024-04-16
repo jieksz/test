5fd5ea465edaee8ea2bf3e87997e3e45a67479a3
jieksz <jieksz@163.com>
2024-04-16 16:02:02 +0800

    上课成果

---
+++ Demo17.java
@@ -0,0 +1,8 @@
+public class Demo17 {
+    public static void main(String[] args) {
+        int test1 = 1;
+        int test2 = 2;
+        int done = (test1 > test2) ? test1 : test2;
+        System.out.println(done);
+    }
+}
---
+++ Demo18.java
@@ -0,0 +1,14 @@
+public class Demo18 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int sum = 0;
+        if (num1 > num2) {
+            sum = num1;
+            System.out.println(sum);
+        } else {
+            sum = num2;
+            System.out.println(sum);
+        }
+    }
+}
