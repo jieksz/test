ba1cfeca72464d6c47b0460f1dcc88cd0a584b4a
jie <jieksz@163.com>
2024-05-21 15:50:09 +0800

    增加构造方法

--- Demo25.java
+++ Demo25.java
@@ -1,3 +1,29 @@
+class Person {
+    private int age;
+    private String name;
+
+    public void setage(int a) {
+        age = a;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public void setname(String n) {
+        name = n;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public Person() {
+        age = 18;
+        name = "张三";
+    }
+}
+
 public class Demo25 {
 
     static int GetSum(int n) {
@@ -10,6 +36,10 @@ public class Demo25 {
 
     public static void main(String[] args) {
         int sum = GetSum(4);
-        System.out.println("sum="+ sum);
+        System.out.println("sum=" + sum);
+
+        Person p = new Person();
+        System.out.println(p.getage());
+        System.out.println(p.getname());
     }
 }
